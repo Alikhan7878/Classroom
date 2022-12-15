db8ca27dcec99ddf4ca92b33c31291ce85707bfe
Alikhan7878 <120654291+Alikhan7878@users.noreply.github.com>
2022-12-15 20:06:02 +0600

    Add files via upload

---
+++ задание 1.js
@@ -0,0 +1,18 @@
+const student = {
+    stack: ["HTML"],
+    level: 1,
+    improveLevel() {
+        let fullStack = ["HTML", "CSS", "JavaScript", "React", "NodeJS"];
+        if (this.level < 5) {
+            this.stack.push(fullStack[this.level++]);
+        }
+        return this;
+    }
+};
+
+student
+    .improveLevel()
+    .improveLevel()
+    .improveLevel();
+
+console.log(student);
